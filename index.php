b8a54f91cf2aa1c5afbe46ce35dab7dbbbd03592
Adam Vlček <vlcek-adam@seznam.cz>
2024-06-15 20:26:10 +0200

    Linked Controller class

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Gamelist\Controllers\Todo;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -11,14 +13,13 @@ $app->addErrorMiddleware(true, true, false);
 
 define('BASE_PATH', __DIR__);
 
+$container = $app->getContainer();
+
 $app->get('/gamelist/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
 
-$app->get('/gamelist/firstPage', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("<h1>this is the first page</h1>");
-    return $response;
-});
+$app->get('/gamelist/todo', [Todo::class, 'view']);
 
 $app->run();
---
+++ src/Controllers/ToDo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Gamelist\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Todo
+{
+    function view(Request $request, Response $response): Response
+    {
+        require 'vendor/autoload.php';
+        ob_start();
+        
+        include "templates/index.html";
+        $html = ob_get_clean();
+        $response->getBody()->write($html);
+        return $response;
+    }
+}
