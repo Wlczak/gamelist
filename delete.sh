a5f19b32df36a5777f37a3be6009c6fd7b0bb31e
Adam Vlček <79417338+Wlczak@users.noreply.github.com>
2024-06-19 09:10:46 +0000

    delete script

---
+++ delete.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Stop all running containers
+echo "Stopping all running containers..."
+docker stop $(docker ps -q)
+
+# Remove all containers
+echo "Removing all containers..."
+docker rm $(docker ps -a -q)
+
+echo "All Docker containers have been removed."
