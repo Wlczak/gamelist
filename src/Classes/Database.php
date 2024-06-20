a8fa5eda2afe735666869e5a9d3feb31b39e2cde
Adam Vlček <vlcek-adam@seznam.cz>
2024-06-20 17:56:14 +0200

    localtest

--- src/Classes/Database.php
+++ src/Classes/Database.php
@@ -6,12 +6,13 @@ use mysqli;
 
 class Database
 {
-    public $hostname = "192.168.0.106";
+    public $hostname = "localhost:3306";
     public $username = "gamelist";
-    public $password = "TpA8kEiUv3W2A@1l";
+    public $password = "todolist";
     public $database = "gamelist";
     function query($array)
     {
         $conn = new mysqli($this->hostname, $this->username, $this->password, $this->database);
+        $conn->close();
     }
 }
