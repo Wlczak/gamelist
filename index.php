c764c491ef3818d07c0fdd9092edd25c6ef1ac85
Adam Vlček <vlcek-adam@seznam.cz>
2024-06-21 11:04:00 +0200

    added cloud sourced styles and js

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="src/script/main.js"></script>
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <title>Gamelist {WIP}</title>
 </head>
 
@@ -15,7 +16,7 @@
         <p>This is the website that gamelist {name subject to change} will be hosted on.<br>For now you can go and try running your own instance of this webapp from the dev branche on <a href="https://github.com/Wlczak/gamelist">GitHub</a></p>
     </div>
 
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 </body>
 
 </html>
