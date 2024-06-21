81e285815dce80aaf68cfe83cd8275ae5103dbb2
Adam Vlček <vlcek-adam@seznam.cz>
2024-06-21 11:01:46 +0200

    added bootstrap and formating

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gamelist</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="src/script/main.js"></script>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
+    <title>Gamelist {WIP}</title>
 </head>
+
 <body>
-    <h1>Gamelist {WIP}</h1>
-    <p>This is the website that gamelist {name subject to change} will be hosted on.<br>For now you can go and try running your own instance of this webapp from the dev branche on <a href="https://github.com/Wlczak/gamelist">GitHub</a></p>
+    <div class="container text-left mt-5 col-6">
+        <h1>Gamelist {WIP}</h1>
+        <p>This is the website that gamelist {name subject to change} will be hosted on.<br>For now you can go and try running your own instance of this webapp from the dev branche on <a href="https://github.com/Wlczak/gamelist">GitHub</a></p>
+    </div>
+
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
