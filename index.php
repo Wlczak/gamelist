a13b677f1949d401a3229f18482538bf3ecd5d15
Adam Vlček <vlcek-adam@seznam.cz>
2024-06-21 10:53:58 +0200

    added index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gamelist</title>
+</head>
+<body>
+    <h1>Gamelist {WIP}</h1>
+    <p>This is the website that gamelist {name subject to change} will be hosted on.<br>For now you can go and try running your own instance of this webapp from the dev branche on <a href="https://github.com/Wlczak/gamelist">GitHub</a></p>
+</body>
+</html>
