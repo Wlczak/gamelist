bf3f320966f9e0cde71d50af7fc582814edb9f4d
Adam Vlček <vlcek-adam@seznam.cz>
2024-06-22 12:18:13 +0200

    Moved js api calls to another file

---
+++ src/script/api.js
@@ -0,0 +1,23 @@
+export class Api {
+    apiQuery(array) {
+        var data = JSON.stringify(array);
+        return fetch(window.location.protocol + "//" + window.location.hostname + "/gamelist/api", {
+            // Added return here
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: data,
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Handle the response from the PHP script if needed
+                return data;
+            })
+            .catch((error) => {
+                // Handle any errors that occurred during the fetch or processing
+                console.error("Error in API call:", error);
+                throw error; // Re-throw the error to propagate it to the caller
+            });
+    }
+}
--- src/script/main.js
+++ src/script/main.js
@@ -1,28 +1,8 @@
+import { Api } from "./api.js";
+var api = new Api();
 var array = {
-    requestType: "dbQuery"
+    requestType: "getList",
 };
-apiQuery(array).then((data) => {
+api.apiQuery(array).then((data) => {
     console.log(data);
 });
-
-function apiQuery(array) {
-    var data = JSON.stringify(array);
-    return fetch(window.location.protocol + "//" + window.location.hostname + "/gamelist/api", {
-        // Added return here
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: data,
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            // Handle the response from the PHP script if needed
-            return data;
-        })
-        .catch((error) => {
-            // Handle any errors that occurred during the fetch or processing
-            console.error("Error in API call:", error);
-            throw error; // Re-throw the error to propagate it to the caller
-        });
-}
