76685f2add37598e4515af213078e73bdb2de701
Adam Vlček <vlcek-adam@seznam.cz>
2024-06-29 19:56:59 +0200

    update login return api

--- src/Classes/Session.php
+++ src/Classes/Session.php
@@ -12,10 +12,21 @@ class Session
         if (key_exists("authError", $_SESSION)) {
             $response['authError'] = $_SESSION['authError'];
             unset($_SESSION['authError']);
-
+        }
+        if (key_exists("authMsg", $_SESSION)) {
             $response['authMsg'] = $_SESSION['authMsg'];
             unset($_SESSION['authMsg']);
         }
+        if (key_exists("authToken", $_SESSION)) {
+            $response['authToken'] = $_SESSION['authToken'];
+            unset($_SESSION['authToken']);
+        }
+        //var_dump($_COOKIE);
+        if (key_exists("authToken", $_COOKIE)) {
+            $response['cookieTest'] = $_COOKIE['authToken'];
+            unset($_COOKIE['authToken']);
+        }
+
         return $response;
     }
 }
