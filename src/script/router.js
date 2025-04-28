910bf309efa1b197191170074e682c059716fefd
Adam Vlček <vlcek-adam@seznam.cz>
2025-04-28 18:04:30 +0200

    got routing working

--- src/script/router.js
+++ src/script/router.js
@@ -2,8 +2,13 @@ import { Swapper } from "./body_swapper.js";
 
 export class Router {
     swapper;
+    routes = [];
     constructor() {
         this.swapper = new Swapper();
+
+        window.addEventListener("pushstate", () => {
+            this.run();
+        });
     }
 
     async fetch_body() {
@@ -13,13 +18,33 @@ export class Router {
             await this.swapper.fetch_body(
                 window.location.origin + "/components/" + window.location.pathname.split("/").pop()
             );
-            console.log("hi")
+            console.log("hi");
         }
     }
 
-    addPath(path, callback) {
-        if (window.location.pathname == path) {
-            this.fetch_body().then(() => callback());
+    run() {
+        this.routes.forEach((element) => {
+            if (window.location.pathname == element.path) {
+                this.fetch_body().then(() => element.callback());
+            }
+        });
+    }
+
+    addPath(path, callback, addButton = false, name = "") {
+        this.routes.push({ path: path, callback: callback });
+        if (addButton) {
+            const button = document.createElement("button");
+            if (name != "") {
+                button.innerHTML = name;
+            } else {
+                button.innerHTML = path.split("/").pop();
+            }
+            button.className = "btn btn-primary";
+            button.addEventListener("click", () => {
+                history.pushState({}, "", path);
+                window.dispatchEvent(new Event("pushstate"));
+            });
+            document.getElementById("nav-buttons").appendChild(button);
         }
     }
 }
--- src/script/todo.js
+++ src/script/todo.js
@@ -6,29 +6,36 @@ var api = new Api();
 var render = new Render(api);
 var router = new Router();
 
-router.addPath("/", () => {
-    render.refreshTasks();
+router.addPath(
+    "/",
+    () => {
+        render.refreshTasks();
 
-    api.getPoints().then((response) => {
-        render.changeScore(response.points);
-    });
-    document.getElementById("addTaskButton").addEventListener("click", (e) => {
-        var score = document.getElementById("scoreValue").value;
-        var content = document.getElementById("taskName").value;
+        api.getPoints().then((response) => {
+            render.changeScore(response.points);
+        });
+        document.getElementById("addTaskButton").addEventListener("click", (e) => {
+            var score = document.getElementById("scoreValue").value;
+            var content = document.getElementById("taskName").value;
 
-        if (score == "" || content == "") {
-            render.showToast("Please fill out all inputs", "red");
-        } else {
-            api.createTask(content, score).then((response) => {
-                if (response.msg == undefined) {
-                    render.showToast("Failed adding new task", "red");
-                } else {
-                    render.showToast("Task added succesfully", "green");
-                    render.refreshTasks();
-                }
-            });
-        }
-    });
-});
+            if (score == "" || content == "") {
+                render.showToast("Please fill out all inputs", "red");
+            } else {
+                api.createTask(content, score).then((response) => {
+                    if (response.msg == undefined) {
+                        render.showToast("Failed adding new task", "red");
+                    } else {
+                        render.showToast("Task added succesfully", "green");
+                        render.refreshTasks();
+                    }
+                });
+            }
+        });
+    },
+    true,
+    "Home"
+);
 
-router.addPath("/shop", {});
+router.addPath("/shop", () => {}, true, "Shop");
+
+router.run();
